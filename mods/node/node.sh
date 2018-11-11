2e34ecb60c6ee21b643da8c637bc80ef47ee99c8
Is Ma <dotpointing@gmail.com>
2018-11-11 17:23:23 -0600

    node, ruby fixed: echo -n

--- mods/node/node.sh
+++ mods/node/node.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # test: node --version | grep $node_version
-echo -n "- mods/node/node.sh v$node_version"
+echo "- mods/node/node.sh v$node_version"
 nvm install $node_version > /dev/null 2>&1
 nvm use $node_version > /dev/null 2>&1
 sudo ln -sf /home/deploy/.nvm/versions/node/v$node_version/bin/node /usr/local/bin/node
--- mods/ruby/ruby.sh
+++ mods/ruby/ruby.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # test: ruby --version | grep $ruby_version
-echo -n "- mods/ruby/ruby.sh v$ruby_version"
+echo "- mods/ruby/ruby.sh v$ruby_version"
 sudo apt-get install -y build-essential libssl-dev libreadline-dev zlib1g-dev > /dev/null 2>&1
 rbenv install $ruby_version > /dev/null 2>&1
 rbenv global $ruby_version
