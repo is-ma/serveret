f15018e25b2d857e415d09bb09ad404718d3a5bd
Is Ma <dotpointing@gmail.com>
2020-04-06 20:24:34 -0500

    Allow PG remote connections (protect servers with ufw).

--- mods/pg/allow_remote_access.sh
+++ mods/pg/allow_remote_access.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
+source ~/.serveret/serveret.conf
+
 # test: grep "^host all $USER 0.0.0.0/0 trust" $HBA_CONF_FILE_PATH
 echo "  * mods/pg/allow_remote_access.sh"
 PG_CONF_FILE_PATH=/home/$USER/.asdf/installs/postgres/$pg_version/data/postgresql.conf
 HBA_CONF_FILE_PATH=/home/$USER/.asdf/installs/postgres/$pg_version/data/pg_hba.conf
 sed -i "s/#listen_addresses/listen_addresses/" $PG_CONF_FILE_PATH
 sed -i "s/listen_addresses = 'localhost'/listen_addresses = '*'/" $PG_CONF_FILE_PATH
-echo "host all $USER 0.0.0.0/0 trust" >> $HBA_CONF_FILE_PATH
+echo "host all all 0.0.0.0/0 trust" >> $HBA_CONF_FILE_PATH
