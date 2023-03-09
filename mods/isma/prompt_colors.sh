b446c4e0c2a0bbeba1e549b7d036af9779b9b053
Is Ma <dotpointing@gmail.com>
2023-03-09 23:34:52 +0000

    Improve prompt_colors documentation

--- mods/isma/prompt_colors.sh
+++ mods/isma/prompt_colors.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 echo "- mods/isma/prompt_colors.sh"
 git clone https://github.com/is-ma/prompt_colors.git ~/.prompt_colors > /dev/null 2>&1
-echo -e "\nsource ~/.prompt_colors/git_prompt.sh" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "# is-ma/prompt_colors" >> ~/.bashrc
+echo "source ~/.prompt_colors/git_prompt.sh" >> ~/.bashrc
 echo "source ~/.prompt_colors/bash_colors.sh" >> ~/.bashrc
 echo "source ~/.prompt_colors/ps1.sh" >> ~/.bashrc
 source ~/.bashrc
