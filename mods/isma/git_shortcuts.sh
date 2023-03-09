262ee854eeb9d262a9839058a9461fbaaf1c3267
Is Ma <dotpointing@gmail.com>
2023-03-09 23:39:13 +0000

    Improve git_shortcuts documentation

--- mods/isma/git_shortcuts.sh
+++ mods/isma/git_shortcuts.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 echo "- mods/isma/git_shortcuts.sh"
 git clone https://github.com/is-ma/git_shortcuts.git ~/.git_shortcuts > /dev/null 2>&1
-echo -e "\nsource ~/.git_shortcuts/git_shortcuts.sh" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "# is-ma/git_shortcuts" >> ~/.bashrc
+echo "source ~/.git_shortcuts/git_shortcuts.sh" >> ~/.bashrc
 source ~/.bashrc
