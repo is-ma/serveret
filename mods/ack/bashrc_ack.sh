40a0705aa3a7c61e1e5d9d27a3926348b7222455
Is Ma <dotpointing@gmail.com>
2023-03-14 12:45:58 -0600

    Update mods/ack installation

--- mods/ack/bashrc_ack.sh
+++ mods/ack/bashrc_ack.sh
@@ -1,5 +1,4 @@
 
-
 ### SERVERET: ACK ###
 alias ackf='ack --smart-case --type-add=frontend:ext:js,coffee,scss,css,haml,html,yml,md,txt --type=frontend'
 alias ackr='ack --smart-case --type-add=rubyrails:ext:gitignore,js,coffee,scss,css,rb,erb,haml,html,yml,md,txt --type=rubyrails --ignore-dir=bin/ --ignore-dir=lib/ --ignore-dir=log/ --ignore-dir=public/assets/ --ignore-dir=tmp/ --ignore-dir=vendor/ --ignore-file=ext:keep,ico,cache,ackrc --ignore-file=is:package.json'
