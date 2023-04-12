effc1e72d835d835b1f45b18cec4638f356dd936
Is Ma <dotpointing@gmail.com>
2023-04-12 17:36:45 -0500

    Fix: ~/.tool-versions

--- mods/node/node.sh
+++ mods/node/node.sh
@@ -6,7 +6,7 @@ echo "- mods/node/node.sh v$node_version"
 
 # install node
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git > /dev/null 2>&1
-echo "nodejs $node_version" >> .tool-versions
+echo "nodejs $node_version" >> ~/.tool-versions
 asdf install > /dev/null 2>&1
 
 # be sure Phusion Passenger and Rails app could use the javascript runtime
--- mods/ruby/ruby.sh
+++ mods/ruby/ruby.sh
@@ -5,7 +5,7 @@ source ~/.serveret/serveret.conf
 echo "- mods/ruby/ruby.sh v$ruby_version"
 
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git > /dev/null 2>&1
-echo "ruby $ruby_version" >> .tool-versions
+echo "ruby $ruby_version" >> ~/.tool-versions
 echo 'gem: --no-document' >> ~/.gemrc
 
 asdf install > /dev/null 2>&1
