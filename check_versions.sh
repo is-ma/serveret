fb5c974ddca5eecd66cbb5b906aa480849830e76
Is Ma <dotpointing@gmail.com>
2023-05-19 15:23:05 +0000

    Beautify check_versions.sh

--- check_versions.sh
+++ check_versions.sh
@@ -1,43 +1,43 @@
-echo -e "\n***** ***** ***** ***** *****  UBUNTU"
+echo -e "\n***** ***** ***** ***** *****  UBUNTU  ***** ***** *****"
 cat /etc/issue
 
-echo -e "\n***** ***** ***** ***** *****     GIT"
+echo -e "\n***** ***** ***** ***** *****     GIT  ***** ***** *****"
 git --version
 
-echo -e "\n***** ***** ***** ***** *****    DATE"
+echo -e "\n***** ***** ***** ***** *****    DATE  ***** ***** *****"
 date
 
-echo -e "\n***** ***** ***** ***** *****     ACK"
+echo -e "\n***** ***** ***** ***** *****     ACK  ***** ***** *****"
 ack --version
 
-echo -e "\n***** ***** ***** ***** *****    ASDF"
+echo -e "\n***** ***** ***** ***** *****    ASDF  ***** ***** *****"
 asdf --version
 
-echo -e "\n***** ***** ***** ***** ***** POSTGRE SQL"
+echo -e "\n***** ***** ***** ***** ***** POSTGRE SQL  ***** ***** *****"
 psql --version
 pg_status
 
-echo -e "\n***** ***** ***** ***** *****    RUBY"
+echo -e "\n***** ***** ***** ***** *****    RUBY  ***** ***** *****"
 ruby --version
 
-echo -e "\n***** ***** ***** ***** ***** BUNDLER"
+echo -e "\n***** ***** ***** ***** ***** BUNDLER  ***** ***** *****"
 bundler --version
 
-echo -e "\n***** ***** ***** ***** *****    NODE"
+echo -e "\n***** ***** ***** ***** *****    NODE  ***** ***** *****"
 node --version
 
-echo -e "\n***** ***** ***** ***** *****   RAILS"
+echo -e "\n***** ***** ***** ***** *****   RAILS  ***** ***** *****"
 rails --version
 
-echo -e "\n***** ***** ***** ***** ***** VARNISH"
+echo -e "\n***** ***** ***** ***** ***** VARNISH  ***** ***** *****"
 sudo service varnish status | head -n5
 
-echo -e "\n***** ***** ***** ***** *****     UFW"
+echo -e "\n***** ***** ***** ***** *****     UFW  ***** ***** *****"
 sudo ufw status
 
-echo -e "\n***** ***** ***** ***** *****   NGINX"
+echo -e "\n***** ***** ***** ***** *****   NGINX  ***** ***** *****"
 sudo service nginx status | head -n5
 # sudo /usr/sbin/passenger-memory-stats
 
-echo -e "\n***** ***** ***** ***** ***** CRONTAB"
+echo -e "\n***** ***** ***** ***** ***** CRONTAB  ***** ***** *****"
 crontab -l
