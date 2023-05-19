667593007f9c7f4e0be9dfb3921063f537550960
Is Ma <dotpointing@gmail.com>
2023-05-19 15:17:57 +0000

    PG: replace multi-echo with heredoc

--- mods/pg.sh
+++ mods/pg.sh
@@ -16,12 +16,14 @@ pg_stop=$(echo "$PG_CTL_PATH -l /home/$USER/pg_log -D $PG_DATA_PATH stop")
 pg_restart=$(echo "$PG_CTL_PATH -l /home/$USER/pg_log -D $PG_DATA_PATH restart")
 
 # aliases
-echo "" >> ~/.bashrc
-echo "### POSTGRE SQL ###" >> ~/.bashrc
-echo "alias pg_status='$pg_status'" >> ~/.bashrc
-echo "alias pg_start='$pg_start'" >> ~/.bashrc
-echo "alias pg_stop='$pg_stop'" >> ~/.bashrc
-echo "alias pg_restart='$pg_restart'" >> ~/.bashrc
+cat << EOF >> ~/.bashrc
+
+POSTGRE SQL ###"
+alias pg_status='$pg_status'
+alias pg_start='$pg_start'
+alias pg_stop='$pg_stop'
+alias pg_restart='$pg_restart'
+EOF
 
 # crontab
 crontab -l | { cat; echo "@reboot $PG_CTL_PATH -l /home/$USER/pg_log -D $PG_DATA_PATH start"; } | crontab - > /dev/null 2>&1
